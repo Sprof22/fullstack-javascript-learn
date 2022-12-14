9144b15dce0207639acdc3c96f159ffb4f36319a
Sprof22 <richmondelaigwu@gmail.com>
2022-12-14 18:15:09 +0100

    check later

--- JavaScript and the DOM/app.js
+++ JavaScript and the DOM/app.js
@@ -1,5 +1,16 @@
-const body = document.body
+const btnUpdate = document.getElementById('btn-main')
+const headline = document.getElementById('headline');
+const items = document.getElementsByTagName('li');
+const highlights = document.getElementsByClassName('highlights')
 
-document.body.addEventListener('click', () => {
-    body.innerHTML = '<h1>You clicked the body</h1>'
+
+
+for(let i = 0; i<items.length; i++){
+    items[i].style.color = 'orchid'
+}
+
+btnUpdate.addEventListener('click', () => {
+    headline.style.border = 'solid 2px red';
+    headline.style.fontSize = '80px';
 });
+
