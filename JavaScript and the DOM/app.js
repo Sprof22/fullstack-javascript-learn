cdf0d28aac06c68fc8b3bcd8f972b493459643db
Sprof22 <richmondelaigwu@gmail.com>
2022-12-17 23:06:09 +0100

    toggle button

--- JavaScript and the DOM/app.js
+++ JavaScript and the DOM/app.js
@@ -1,11 +1,29 @@
 const btnUpdate = document.querySelector('.btn-main');
-
+const btnToggle = document.querySelector('.btn-toggle')
+console.log('we here')
 
 btnUpdate.addEventListener('click', () => {
- const headline = document.getElementById('headline');
- const input = document.querySelector('.input-main');
+    const headline = document.getElementById('headline');
+    const input = document.querySelector('.input-main');
 
-heading.textContent = input.value
-input.value = '';
+    headline.textContent = input.value;
+    input.value= '';
 });
 
+btnToggle.addEventListener('click', () => {
+    const listContainer = document.querySelector('.list-container');
+    if(listContainer.style.display === 'block'){
+        listContainer.style.display = 'none'
+    }   else {
+        listContainer.style.display = 'block'
+    };
+})
+// btnUpdate.addEventListener('click', () => {
+//  const headline = document.getElementById('headline');
+//  const input = document.querySelector('.input-main');
+
+//  console.log(input.value);
+
+// headline.textContent = input.value;
+// input.value = '';
+// });
