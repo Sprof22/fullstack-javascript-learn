91ef2c4e9c5b99bf83a972749717f93eb1782bf7
Sprof22 <richmondelaigwu@gmail.com>
2022-12-28 14:19:49 +0100

    call back prm

--- Async/js/callbacks.js
+++ Async/js/callbacks.js
@@ -32,11 +32,15 @@ function generateHTML(data) {
   
 }
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (event) => {
   getJSON(astrosUrl, (json) => {
       json.people.map(person => {
         getJSON(wikiUrl + person.name, generateHTML)
       });
   })
-});
+
+  event.target.remove();
+})
+
+
 
---
+++ Async/js/ghjh.js
@@ -0,0 +1,8 @@
+// else {
+//     section.innerHTML = `
+//       <img src="img/profile.jpeg" alt="ocean clouds seen from space">
+//       <h2>${data.title}</h2>
+//       <p>Results unavailable for ${data.title}</p>
+//       ${data.extract_html}
+//     `;
+//   }
