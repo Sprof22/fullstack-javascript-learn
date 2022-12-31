f68619e6b2a2a9732f6c4c911a9a72a6e34d6323
Sprof22 <richmondelaigwu@gmail.com>
2022-12-31 23:34:22 +0100

    fix terminal

--- Async/js/promises-breakfast.js
+++ Async/js/promises-breakfast.js
@@ -1,6 +1,6 @@
 const breakfastPromise = new Promise ((resolve, reject) => {
     setTimeout(() => {
-        reject('your order is ready, come and get it')
+        resolve('your order is ready, come and get it')
     }, 3000);
 });
 
