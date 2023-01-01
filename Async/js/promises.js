e767653774cb37e0cbd5852117c6ff2ab201001f
Sprof22 <richmondelaigwu@gmail.com>
2023-01-01 12:12:35 +0100

    small fix

--- Async/js/promises.js
+++ Async/js/promises.js
@@ -3,16 +3,21 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+function getJSON(url) {
+  return new Promise((resolve, reject)=>{
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if(xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        resolve(data);
+      } else {
+        reject( Error(xhr.statusText))
+      }
+    };
+    xhr.onerror = () => reject(Error('A network error occured'));
+    xhr.send();
+  })
 }
 
 function getProfiles(json) {
@@ -44,6 +49,9 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
+  getJSON(astrosUrl)
+  .then(getProfiles)
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
   event.target.remove();
 });
