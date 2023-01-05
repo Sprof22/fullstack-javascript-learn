e4b149dea23f06b450ef5bb68c9dcb3dab785bd3
Sprof22 <richmondelaigwu@gmail.com>
2023-01-05 23:51:50 +0100

    promise.all

--- Fetch/js/app.js
+++ Fetch/js/app.js
@@ -7,8 +7,22 @@ const form = document.querySelector('form');
 // ------------------------------------------
 function fetchData(url) {
     return fetch(url)
+        .then(checkStatus)
         .then(res => res.json())
+        .catch(error => console.log('Looks like there was a problem'), error)
 }
+
+Promise.all([
+    fetchData('https://dog.ceo/api/breeds/list'),
+    fetchData('https://dog.ceo/api/breeds/image/random')
+])
+.then(data => {
+    const breedList = data[0].message;
+    const randomImage = data[1].message;
+
+    generateOptions(breedList);
+    generateImage(randomImage);
+})
 
 fetchData('https://dog.ceo/api/breeds/list')
 .then(data => generateOptions(data.message))
@@ -19,6 +33,13 @@ fetchData('https://dog.ceo/api/breeds/image/random')
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+    function checkStatus(response){
+        if (response.ok){
+            return Promise.resolve(response)
+        } else {
+            return Promise.reject(new Error(response.statusText))
+        }
+    }
     function generateOptions(data){
         const options = data.map(item => `
             <option value='${item}'>${item}</option>
