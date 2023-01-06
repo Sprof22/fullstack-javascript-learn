9bb74a61ba3214f3d2bb792f6befe87d1ccc2d7d
Sprof22 <richmondelaigwu@gmail.com>
2023-01-06 20:00:28 +0100

    methods and methodology

--- OOJS/Object-specific/Pets.js
+++ OOJS/Object-specific/Pets.js
@@ -1,12 +1,19 @@
 class Pet {
-    constructor(animal, age, breed){
+    constructor(animal, age, breed, sound){
         this.animal = animal;
         this.age = age;
         this.breed = breed;
+        this.sound = sound;
 
+    }
+
+    sound(){
+        console.log(this.sound);
     }
 }
 
-const ernie = new Pet('lion', 2, 'wolf');
+const ernie = new Pet('simba', 2, 'lion', 'roar');
+const wazoo = new Pet('dog', 4, 'dog', 'bark');
 
-console.log(ernie.animal);
+console.log(ernie);
+console.log(wazoo);
---
+++ OOJS/Object-specific/Practice.js
@@ -0,0 +1,16 @@
+class User {
+    constructor(email, username, birthday) {
+        this.email = email;
+        this.username = username;
+        this.birthday = birthday;
+    }
+  
+  changeUsername(username){
+    this.username = username;
+  }
+}
+
+var user1 = new User('JavaScriptStudent@teamtreehouse.com', 'TreehouseStudent2', '1/08/1991');
+
+user1.changeUsername();
+console.log(user1, 'this good')
