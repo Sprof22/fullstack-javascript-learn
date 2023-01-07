ae8c6c3b82c609656eaff4ab32eec70804f03ae3
Sprof22 <richmondelaigwu@gmail.com>
2023-01-07 14:31:36 +0100

    we ready

--- OOJS/Object-specific/Pets.js
+++ OOJS/Object-specific/Pets.js
@@ -7,8 +7,45 @@ class Pet {
 
     }
 
+    get activity(){
+        const today = new Date();
+        const hour = today.getHours();
+
+        if (hour > 8 && hour <= 20 ){
+            return 'playing'
+
+        } else {
+            return 'sleeping'
+        }
+
+    }
+    get owner () {
+        return this._owner;
+    }
+
+    set owner (owner){
+        this._owner = owner;
+        console.log('setter called')
+    }
+
     sound(){
         console.log(this.sound);
+    }
+}
+
+class Owner {
+    constructor (name , address){
+        this.name = name;
+        this.address = address;
+    }
+    
+    set phone(phone){
+        const phoneNormalized = phone.replace(/[^0-9]/g, '')
+        this._phone = phoneNormalized;
+    }
+
+    get phone (){
+        return this._phone
     }
 }
 
@@ -17,3 +54,8 @@ const wazoo = new Pet('dog', 4, 'dog', 'bark');
 
 console.log(ernie);
 console.log(wazoo);
+
+ernie.owner= new Owner('Big Shaq', '1 Nyam Shom');
+ernie.owner.phone = '(+234) 8133122'
+
+console.log(ernie.owner);
