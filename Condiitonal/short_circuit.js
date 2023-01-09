dc60d070571c87ee5999a1994c75aab3590c8cd7
Sprof22 <richmondelaigwu@gmail.com>
2023-01-09 16:05:02 +0100

    cracking conditionals

--- Condiitonal/short_circuit.js
+++ Condiitonal/short_circuit.js
@@ -2,11 +2,19 @@ function isAdult(age){
     return age && age >= 18
 }
 
-console.log(isAdult(15));
+// console.log(isAdult(15));
 
 function countToFive(start){
     start = start || 1;
-    for (var i = start; i <= 5; i++){
-        console.log(i)
+    if (start && start < 5){
+        for (var i = start; i <= 5; i++){
+            console.log(i)
+        }
+    } else if (start && start > 5){
+        for (var i = start; i >= 5; i--){
+            console.log(i)
+        }
     }
 }
+
+countToFive();
